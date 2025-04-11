0f88552ca34e56944f6bd2ff7305a389d2cbbe02
achref2008 <157286665+achref2008@users.noreply.github.com>
2025-04-11 23:58:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  document.getElementById('formMessage').textContent = "Thanks! I’ll get back to you soon 😊";
+  this.reset();
+});
