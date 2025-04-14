93a90facf15d03c38461ecb0e7a46920000f627b
achref2008 <157286665+achref2008@users.noreply.github.com>
2025-04-14 22:57:13 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-  e.preventDefault();
-  document.getElementById('formMessage').textContent = "Thanks! I’ll get back to you soon 😊";
-  this.reset();
-});
